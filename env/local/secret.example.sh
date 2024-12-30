dc723b02328e25334856eb30d727ba3c769897a2
Adrian Webb <adrian@webb.sh>
2024-12-29 20:25:33 -0500

    Adding an initial secret.example environment script.

---
+++ env/local/secret.example.sh
@@ -0,0 +1,62 @@
+#
+# ArgoCD environment variables
+#
+export ARGOCD_SERVER_SECRET="999999999"
+export ARGOCD_ADMIN_PASSWORD="admin"
+
+#
+# Mailgun environment variables
+#
+export MAILGUN_DOMAIN="example.com"
+export MAILGUN_API_KEY=""
+export MAILGUN_WEBHOOK_KEY=""
+
+#
+# Qdrant environment variables
+#
+export QDRANT_PASSWORD="999999999"
+
+#
+# Grafana environment variables
+#
+export GRAFANA_ADMIN_USER="admin"
+export GRAFANA_ADMIN_PASSWORD="YzhlajFwQXFwbzVZcDF6YXBpelpIWVg2U3FSREJYd0F4SEZYcFVFVg==" # admin
+
+#
+# Knowledge Manager environment configurations
+#
+export KNOWLEDGE_ADMIN_SUPERUSER_EMAIL="somebody@example.com"
+export KNOWLEDGE_ADMIN_SUPERUSER_FIRST_NAME=""
+export KNOWLEDGE_ADMIN_SUPERUSER_LAST_NAME=""
+export KNOWLEDGE_ADMIN_SUPERUSER_PASSWORD="999999999"
+export KNOWLEDGE_ADMIN_SUPERUSER_API_KEY="999999999"
+
+export KNOWLEDGE_ADMIN_SECRET_KEY="999999999"
+
+export KNOWLEDGE_ADMIN_FROM_EMAIL="Nobody Home <noreply@example.com>"
+export KNOWLEDGE_ADMIN_CONTACT_EMAIL="hello@example.com"
+
+#
+# Zimagi environment configurations
+#
+export POSTGRESQL_PASSWORD="999999999"
+export REDIS_PASSWORD="999999999"
+
+export ZIMAGI_SECRET_KEY="999999999"
+export ZIMAGI_ADMIN_API_KEY="999999999"
+
+export ZIMAGI_EMAIL_HOST_DOMAIN="smtp.example.com"
+export ZIMAGI_EMAIL_HOST_PORT=465
+export ZIMAGI_EMAIL_HOST_USER="somebody@example.com"
+export ZIMAGI_EMAIL_HOST_PASSWORD=""
+
+export ZIMAGI_GITHUB_ORG=""
+export GITHUB_API_KEY=""
+
+export HUGGINGFACE_API_TOKEN=""
+export DEEPINFRA_API_KEY=""
+
+export ZIMAGI_INTERFACE_REPO_REMOTE="git@github.com:org/repo.git"
+export ZIMAGI_INTERFACE_REPO_REFERENCE="main"
+export ZIMAGI_INTERFACE_REPO_PUBLIC_KEY=""
+export ZIMAGI_INTERFACE_REPO_PRIVATE_KEY=""
