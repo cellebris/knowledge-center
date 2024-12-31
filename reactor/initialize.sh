3239ce2b33d361874ffc7a449b6391a2856e6683
Adrian Webb <adrian@webb.sh>
2024-12-30 20:25:07 -0500

    Adding some Zimagi environment variables in the reactor initialize script.

--- reactor/initialize.sh
+++ reactor/initialize.sh
@@ -4,3 +4,25 @@
 #
 export OPERATIONS_DOMAIN="${OPERATIONS_SUBDOMAIN}.${PRIMARY_DOMAIN}"
 export AUTH_DOMAIN="${AUTH_SUBDOMAIN}.${PRIMARY_DOMAIN}"
+
+export ZIMAGI_APP_NAME="$APP_NAME"
+
+export ZIMAGI_DEFAULT_MODULES="[
+  {
+    \"provider\": \"github\",
+    \"remote\": \"fractalsynapse/nexical-core-engine\",
+    \"reference\": \"main\"
+  }
+]"
+
+export ZIMAGI_SENTENCE_PARSER_PROVIDERS='["core_en_web"]'
+export ZIMAGI_ENCODER_PROVIDERS='["mpnet_di"]'
+export ZIMAGI_SUMMARIZER_PROVIDERS='["mixtral_di_7bx8"]'
+
+export ZIMAGI_DEFAULT_PORTAL="interface"
+export ZIMAGI_PORTALS="{
+  \"interface\": {
+    \"host\": \"https://api.admin.${PRIMARY_DOMAIN}\",
+    \"token\": \"${KNOWLEDGE_ADMIN_SUPERUSER_API_KEY}\"
+  }
+}"
