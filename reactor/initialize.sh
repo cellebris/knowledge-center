e472acd1428e30c8a5b291dbed290c3680798098
Adrian Webb <adrian@webb.sh>
2024-12-30 23:21:06 -0500

    Updating quoting and a few platform environment variables.

--- reactor/initialize.sh
+++ reactor/initialize.sh
@@ -7,22 +7,22 @@ export AUTH_DOMAIN="${AUTH_SUBDOMAIN}.${PRIMARY_DOMAIN}"
 
 export ZIMAGI_APP_NAME="$APP_NAME"
 
-export ZIMAGI_DEFAULT_MODULES="[
+export ZIMAGI_DEFAULT_MODULES='[
   {
-    \"provider\": \"git\",
-    \"remote\": \"https://github.com/cellebris/zimagi-publisher.git\",
-    \"reference\": \"main\"
+    "provider": "git",
+    "remote": "https://github.com/cellebris/zimagi-publisher.git",
+    "reference": "main"
   }
-]"
+]'
 
 export ZIMAGI_SENTENCE_PARSER_PROVIDERS='["core_en_web"]'
 export ZIMAGI_ENCODER_PROVIDERS='["mpnet_di"]'
 export ZIMAGI_SUMMARIZER_PROVIDERS='["mixtral_di_7bx8"]'
 
 export ZIMAGI_DEFAULT_PORTAL="interface"
-export ZIMAGI_PORTALS="{
-  \"interface\": {
-    \"host\": \"https://api.admin.${PRIMARY_DOMAIN}\",
-    \"token\": \"${KNOWLEDGE_ADMIN_SUPERUSER_API_KEY}\"
+export ZIMAGI_PORTALS='{
+  "interface": {
+    "host": "https://api.admin.'${PRIMARY_DOMAIN}'",
+    "token": "'${KNOWLEDGE_ADMIN_SUPERUSER_API_KEY}'"
   }
-}"
+}'
